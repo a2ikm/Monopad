7e329ae93d7ad8373f9ebb81219bdbeaceebef33
Masato Ikeda <masato.ikeda@gmail.com>
2022-06-04 00:14:43 +0900

    Initial Commit

---
+++ Monopad/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Monopad
+//
+//  Created by Masato Ikeda on 2022/06/04.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Monopad/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  Monopad
+//
+//  Created by Masato Ikeda on 2022/06/04.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
